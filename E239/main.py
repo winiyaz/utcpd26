278b8f5f7ba8643ea2258d32c3e693151dc796f3
winiyaz <winiyaz@proton.me>
2024-07-30 10:59:47 -0700

    👍 - Writing

--- E239/main.py
+++ E239/main.py
@@ -4,7 +4,12 @@ with open("f1.txt") as f1:
 	l1 = f1.readlines()
 
 with open("f2.txt") as f2:
-	l2 = f1.readlines()
+	l2 = f2.readlines()
 
+# Find the common elements between l1 and l2
+res = [n for n in l1 if n in l2]
 
-
+# Write the result to a file called res.txt
+with open("res.txt", "w") as f:
+	for n in res:
+		f.write(n)
